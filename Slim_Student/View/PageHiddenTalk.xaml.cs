16c674ae2bf70d0d5fa87cf78d550973f1ba15d4
JangInHyeok <zzangyinhyug803@naver.com>
2016-08-11 18:24:14 +0900

    [채팅] 예외처리 대부분 끝, 서버에서 연결안할시 접속 못하게 하는 예외만 처리해주기

--- Slim_Student/View/PageHiddenTalk.xaml.cs
+++ Slim_Student/View/PageHiddenTalk.xaml.cs
@@ -22,8 +22,8 @@ namespace Slim_Student
         public PageHiddenTalk()
         {
             InitializeComponent();
-
-            DataContext = new ViewModelPageHiddenTalk(this,txtMsg);
+            PortBox.MaxLength = 4;
+            DataContext = new ViewModelPageHiddenTalk(this, txtMsg, PortBox, IDText, ServerConnectingBtn);
             textbox1.IsReadOnly = true;
         }
 
@@ -42,5 +42,37 @@ namespace Slim_Student
                 textbox1.Dispatcher.BeginInvoke(d, new Object[] { nMessage });
             }
         }
+
+        #region portBox를 숫자만 입력하게
+        private string prevText;
+        private void PortBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            double value;
+
+            if (double.TryParse(textBox.Text, out value))
+            {
+                this.prevText = textBox.Text;
+            }
+            else
+            {
+                textBox.Text = this.prevText;
+                textBox.SelectionLength = this.prevSelectionLength;
+                textBox.SelectionStart = this.prevSelectionStart;
+            }
+
+        }
+
+        int prevSelectionStart;
+        int prevSelectionLength;
+        private void PortBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            TextBox textbox = sender as TextBox;
+            this.prevSelectionStart = textbox.SelectionStart;
+            this.prevSelectionLength = textbox.SelectionLength;
+        }
+
+        #endregion
     }
 }
